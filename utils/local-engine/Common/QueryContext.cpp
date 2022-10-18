f347e0c49dbde29ad53c824457ffa46682b3bb79
LiuNeng <1398775315@qq.com>
2022-10-18 13:40:05 +0800

    integrate memory allocator

--- utils/local-engine/Common/QueryContext.cpp
+++ utils/local-engine/Common/QueryContext.cpp
@@ -16,30 +16,40 @@ extern thread_local std::function<void(Int64)> before_free;
 namespace local_engine
 {
 
-thread_local std::shared_ptr<CurrentThread::QueryScope> query_scope;
-thread_local std::shared_ptr<ThreadStatus> thread_status;
+thread_local std::weak_ptr<CurrentThread::QueryScope> query_scope;
+thread_local std::weak_ptr<ThreadStatus> thread_status;
+std::unordered_map<int64_t, NativeAllocatorContextPtr> allocator_map;
 
-DB::ContextPtr initializeQuery()
+int64_t initializeQuery(ReservationListenerWrapperPtr listener)
 {
     auto query_context = Context::createCopy(SerializedPlanParser::global_context);
     query_context->makeQueryContext();
-    if (!thread_status)
-    {
-        thread_status = std::make_shared<ThreadStatus>();
-    }
-    if (!query_scope)
-    {
-        query_scope = std::make_shared<CurrentThread::QueryScope>(query_context);
-    }
-    CurrentMemoryTracker::before_alloc = [](Int64 size) -> void { std::cout << "alloc memory: " + std::to_string(size) << std::endl; };
-    CurrentMemoryTracker::before_free = [](Int64 size) -> void { std::cout << "free memory: " + std::to_string(size)<< std::endl; };
-    return query_context;
+    auto allocator_context= std::make_shared<NativeAllocatorContext>();
+    allocator_context->query_scope = std::make_shared<CurrentThread::QueryScope>(query_context);
+    allocator_context->thread_status = std::make_shared<ThreadStatus>();
+    allocator_context->query_context = query_context;
+    query_scope = std::weak_ptr<CurrentThread::QueryScope>(allocator_context->query_scope);
+    thread_status = std::weak_ptr<ThreadStatus>(allocator_context->thread_status);
+    auto allocator_id = reinterpret_cast<int64_t>(allocator_context.get());
+    allocator_map.emplace(allocator_id, allocator_context);
+    CurrentMemoryTracker::before_alloc = [listener](Int64 size) -> void { listener->reserve(size); };
+    CurrentMemoryTracker::before_free = [listener](Int64 size) -> void { listener->free(size); };
+    return allocator_id;
 }
 
-void shutdownQuery()
+void releaseAllocator(int64_t allocator_id)
 {
-    query_scope.reset();
-    thread_status.reset();
+    allocator_map.erase(allocator_id);
+}
+
+const std::unordered_map<int64_t, NativeAllocatorContextPtr> getAllocatorContextMap()
+{
+    return allocator_map;
+}
+
+int64_t allocatorMemoryUsage(int64_t allocator_id)
+{
+    return getAllocatorContextMap().at(allocator_id)->thread_status->memory_tracker.get();
 }
 
 }
--- utils/local-engine/Common/QueryContext.h
+++ utils/local-engine/Common/QueryContext.h
@@ -1,9 +1,26 @@
 #pragma once
 #include <Interpreters/Context_fwd.h>
+#include <Common/ThreadStatus.h>
+#include <Common/CurrentThread.h>
+#include <jni/ReservationListenerWrapper.h>
+#include <unordered_map>
 
 namespace local_engine
 {
-DB::ContextPtr initializeQuery();
+int64_t initializeQuery(ReservationListenerWrapperPtr listener);
 
-void shutdownQuery();
+void releaseAllocator(int64_t allocator_id);
+
+int64_t allocatorMemoryUsage(int64_t allocator_id);
+
+struct NativeAllocatorContext
+{
+    std::shared_ptr<DB::CurrentThread::QueryScope> query_scope;
+    std::shared_ptr<DB::ThreadStatus> thread_status;
+    DB::ContextPtr query_context;
+};
+
+using NativeAllocatorContextPtr = std::shared_ptr<NativeAllocatorContext>;
+
+const std::unordered_map<int64_t, NativeAllocatorContextPtr> getAllocatorContextMap();
 }
